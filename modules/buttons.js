dae0640f6597ea7fa5931fba4ab63619b414b782
Ladybianca <bianca.addae@gritacademy.se>
2026-03-20 14:59:24 +0100

    Ändrat contact

--- modules/buttons.js
+++ modules/buttons.js
@@ -3,22 +3,7 @@ const menuBtn = document.querySelector('.dropdown-btn')
 const menu = document.querySelector('.dropdown-content')
 
 
-//forms submit
-const formReset = document.querySelector('#card');
-let username;
-const fbtn = document.querySelector('#fbtn');
 
-fbtn.addEventListener('click', async(event)=>{
-     event.preventDefault();
-    username = document.querySelector('#username').value;
-    const date = new Date();
-    console.log(date);
-    const timeZone = document.querySelector('#timeZone');
-    timeZone.textContent = username + ' joined ' + date;
-
-    alert('Wellcome '+ username);
-   formReset.reset();
-})
 
 menuBtn.addEventListener('click', (event) => {
     event.stopPropagation(); 
